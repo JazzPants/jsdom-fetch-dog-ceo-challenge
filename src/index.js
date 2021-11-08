25aad2db2f1d95590798e7e52720dda139d7a2a1
JazzPants <leighton.l14@gmail.com>
2021-11-09 10:43:25 +1100

    First Commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,51 @@
 console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+
+
+function fetchImgs() {
+    return fetch(imgUrl)
+    .then(resp => resp.json())
+    .then(json => json.message);
+}
+
+function fetchBreeds() {
+    return fetch(breedUrl)
+    .then(resp => resp.json())
+    .then(json => json.message);
+}
+
+//need json.message, not json
+document.addEventListener('DOMContentLoaded', () => {
+    fetchImgs().then(x => {
+        x.forEach(img => {
+            let list = document.createElement('div');
+            list.innerHTML = `<img src="${img}" />`;
+            document.querySelector('#dog-image-container').append(list);
+        })
+    })
+
+    fetchBreeds().then (a => {
+        let x = Object.keys(a)
+
+        x.forEach(Breed => {
+            let list = document.createElement('li');
+            list.innerHTML = `${Breed}`; //remove li here to remove the extra dot points `<li>${Breed}<li>`;
+            document.querySelector('#dog-breeds').append(list);
+
+            list.addEventListener('click', (e) => {
+                console.log(e)
+                list.style.color = 'red';
+            })
+
+        })
+    })
+
+})
+
+
+
+
+//string starts with... or use [0] (first letter of string when converted to array)
+//make more efficient (so code doesnt have to delete the original list on the page)
